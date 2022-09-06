d60d5b2b95b252e77d2d88c5ec3a2a4860c0c97d
Anthony Huang <anthonyhuang07@icloud.com>
2022-09-06 14:10:47 -0400

    reclick buttons now

--- src/scripts/text.js
+++ src/scripts/text.js
@@ -10,7 +10,6 @@ const blockBtn = document.getElementById("block");
 let boldOn, italOn, underOn, strikeOn, spoilOn, quoteOn, lineOn, blockOn;
 boldOn = italOn = underOn = strikeOn = spoilOn = quoteOn = lineOn = blockOn = false;
 function resetFn() {
-    boldBtn.style.cursor = italBtn.style.cursor = underBtn.style.cursor = strikeBtn.style.cursor = spoilBtn.style.cursor = quoteBtn.style.cursor = lineBtn.style.cursor = blockBtn.style.cursor = "pointer";
     boldBtn.style.color = italBtn.style.color = underBtn.style.color = strikeBtn.style.color = spoilBtn.style.color = quoteBtn.style.color = lineBtn.style.color = blockBtn.style.color = "white";
     textArea.value = "";
     textArea.style.fontWeight = "normal";
@@ -29,9 +28,8 @@ function copyFn() {
     setTimeout(() => { btn.innerHTML = "Copy"; }, 1000);
 }
 function rmFn() {
-    let newStr = textArea.value.replaceAll(/[*~_|`]|> /g, '');
+    let newStr = textArea.value.replaceAll(/[*`]|> |~~|__|\|\|/g, '');
     textArea.value = newStr;
-    boldBtn.style.cursor = italBtn.style.cursor = underBtn.style.cursor = strikeBtn.style.cursor = spoilBtn.style.cursor = quoteBtn.style.cursor = lineBtn.style.cursor = blockBtn.style.cursor = "pointer";
     boldBtn.style.color = italBtn.style.color = underBtn.style.color = strikeBtn.style.color = spoilBtn.style.color = quoteBtn.style.color = lineBtn.style.color = blockBtn.style.color = "white";
     textArea.style.fontWeight = "normal";
     textArea.style.fontStyle = "";
@@ -44,78 +42,125 @@ function boldFn(text) {
     if (boldOn === false) {
         text = "**" + text.concat("**");
         textArea.style.fontWeight = "bold";
-        boldBtn.style.cursor = "default";
         boldBtn.style.color = 'gray';
         textArea.value = text;
         boldOn = true;
+    }
+    else if (boldOn === true) {
+        text = text.replace(/\*\*/g, "");
+        textArea.style.fontWeight = "normal";
+        boldBtn.style.color = 'white';
+        textArea.value = text;
+        boldOn = false;
     }
 }
 function italFn(text) {
     if (italOn === false) {
         text = "*" + text.concat("*");
         textArea.style.fontStyle = "italic";
-        italBtn.style.cursor = "default";
         italBtn.style.color = 'gray';
         textArea.value = text;
         italOn = true;
+    }
+    else if (italOn === true) {
+        text = text.replace(/\*/g, "");
+        textArea.style.fontStyle = "normal";
+        italBtn.style.color = 'white';
+        textArea.value = text;
+        italOn = false;
     }
 }
 function underFn(text) {
     if (underOn === false) {
         text = "__" + text.concat("__");
         textArea.style.textDecoration = "underline";
-        underBtn.style.cursor = "default";
         underBtn.style.color = 'gray';
         textArea.value = text;
         underOn = true;
+    }
+    else if (underOn === true) {
+        text = text.replace(/__/g, "");
+        textArea.style.textDecoration = "none";
+        underBtn.style.color = 'white';
+        textArea.value = text;
+        underOn = false;
     }
 }
 function strikeFn(text) {
     if (strikeOn === false) {
         text = "~~" + text.concat("~~");
         textArea.style.textDecoration = "line-through";
-        strikeBtn.style.cursor = "default";
         strikeBtn.style.color = 'gray';
         textArea.value = text;
         strikeOn = true;
+    }
+    else if (strikeOn === true) {
+        text = text.replace(/~~/g, "");
+        textArea.style.textDecoration = "none";
+        strikeBtn.style.color = 'white';
+        textArea.value = text;
+        strikeOn = false;
     }
 }
 function spoilFn(text) {
     if (spoilOn === false) {
         text = "||" + text.concat("||");
         textArea.style.color = "#999999";
-        spoilBtn.style.cursor = "default";
         spoilBtn.style.color = 'gray';
         textArea.value = text;
         spoilOn = true;
+    }
+    else if (spoilOn === true) {
+        text = text.replace(/\|\|/g, "");
+        textArea.style.color = "white";
+        spoilBtn.style.color = 'white';
+        textArea.value = text;
+        spoilOn = false;
     }
 }
 function quoteFn(text) {
     if (quoteOn === false) {
         text = "> " + text;
-        quoteBtn.style.cursor = "default";
         quoteBtn.style.color = 'gray';
         textArea.value = text;
         quoteOn = true;
+    }
+    else if (quoteOn === true) {
+        text = text.replace(/> /g, "");
+        quoteBtn.style.color = 'white';
+        textArea.value = text;
+        quoteOn = false;
     }
 }
 function lineFn(text) {
     if (lineOn === false) {
         text = "`" + text.concat("`");
         textArea.style.fontFamily = "monospace";
-        lineBtn.style.cursor = "default";
         lineBtn.style.color = 'gray';
         textArea.value = text;
         lineOn = true;
+    }
+    else if (lineOn === true) {
+        text = text.replace(/`/g, "");
+        lineBtn.style.color = 'white';
+        textArea.style.fontFamily = "JetBrains Mono";
+        textArea.value = text;
+        lineOn = false;
     }
 }
 function blockFn(text) {
     if (blockOn === false) {
         text = "```" + text.concat("```");
         textArea.style.fontFamily = "monospace";
-        blockBtn.style.cursor = "default";
         blockBtn.style.color = 'gray';
         textArea.value = text;
         blockOn = true;
+    }
+    else if (blockOn === true) {
+        text = text.replace(/```/g, "");
+        blockBtn.style.color = 'white';
+        textArea.style.fontFamily = "JetBrains Mono";
+        textArea.value = text;
+        blockOn = false;
     }
 }
--- src/scripts/text.ts
+++ src/scripts/text.ts
@@ -12,7 +12,6 @@ let boldOn: boolean, italOn: boolean, underOn: boolean, strikeOn: boolean, spoil
 boldOn = italOn = underOn = strikeOn = spoilOn = quoteOn = lineOn = blockOn = false
 
 function resetFn(): void{
-    boldBtn.style.cursor = italBtn.style.cursor = underBtn.style.cursor = strikeBtn.style.cursor = spoilBtn.style.cursor = quoteBtn.style.cursor = lineBtn.style.cursor = blockBtn.style.cursor =  "pointer"
     boldBtn.style.color = italBtn.style.color = underBtn.style.color = strikeBtn.style.color = spoilBtn.style.color = quoteBtn.style.color = lineBtn.style.color = blockBtn.style.color =  "white"
     textArea.value = ""
     textArea.style.fontWeight = "normal"
@@ -35,9 +34,8 @@ function copyFn(): void{
 
 }
 function rmFn(): void{
-    let newStr = textArea.value.replaceAll(/[*~_|`]|> /g, '')
+    let newStr = textArea.value.replaceAll(/[*`]|> |~~|__|\|\|/g, '')
     textArea.value = newStr
-    boldBtn.style.cursor = italBtn.style.cursor = underBtn.style.cursor = strikeBtn.style.cursor = spoilBtn.style.cursor = quoteBtn.style.cursor = lineBtn.style.cursor = blockBtn.style.cursor =  "pointer"
     boldBtn.style.color = italBtn.style.color = underBtn.style.color = strikeBtn.style.color = spoilBtn.style.color = quoteBtn.style.color = lineBtn.style.color = blockBtn.style.color =  "white"
     textArea.style.fontWeight = "normal"
     textArea.style.fontStyle = ""
@@ -51,83 +49,119 @@ function boldFn(text: string): void {
     if (boldOn === false) {
         text = "**" + text.concat("**")
         textArea.style.fontWeight = "bold"
-        boldBtn.style.cursor = "default"
         boldBtn.style.color = 'gray';
         textArea.value = text
         boldOn = true
+    } else if(boldOn === true) {
+        text = text.replace(/\*\*/g, "")
+        textArea.style.fontWeight = "normal"
+        boldBtn.style.color = 'white';
+        textArea.value = text
+        boldOn = false
     }
 }
 function italFn(text: string): void {
     if (italOn === false) {
         text = "*" + text.concat("*")
         textArea.style.fontStyle = "italic"
-        italBtn.style.cursor = "default"
         italBtn.style.color = 'gray';
         textArea.value = text
-        
         italOn = true
+    } else if(italOn === true) {
+        text = text.replace(/\*/g, "")
+        textArea.style.fontStyle = "normal"
+        italBtn.style.color = 'white';
+        textArea.value = text
+        italOn = false
     }
 }
 function underFn(text: string): void {
     if (underOn === false) {
         text = "__" + text.concat("__")
         textArea.style.textDecoration = "underline"
-        underBtn.style.cursor = "default"
         underBtn.style.color = 'gray';
         textArea.value = text
         
         underOn = true
+    } else if(underOn === true) {
+        text = text.replace(/__/g, "")
+        textArea.style.textDecoration = "none"
+        underBtn.style.color = 'white';
+        textArea.value = text
+        underOn = false
     }
 }
 function strikeFn(text: string): void {
     if (strikeOn === false) {
         text = "~~" + text.concat("~~")
         textArea.style.textDecoration = "line-through"
-        strikeBtn.style.cursor = "default"
         strikeBtn.style.color = 'gray';
         textArea.value = text
         
         strikeOn = true
+    } else if(strikeOn === true) {
+        text = text.replace(/~~/g, "")
+        textArea.style.textDecoration = "none"
+        strikeBtn.style.color = 'white';
+        textArea.value = text
+        strikeOn = false
     }
 }
 function spoilFn(text: string): void {
     if (spoilOn === false) {
         text = "||" + text.concat("||")
         textArea.style.color = "#999999"
-        spoilBtn.style.cursor = "default"
         spoilBtn.style.color = 'gray';
         textArea.value = text
-        
         spoilOn = true
+    } else if(spoilOn === true) {
+        text = text.replace(/\|\|/g, "")
+        textArea.style.color = "white"
+        spoilBtn.style.color = 'white';
+        textArea.value = text
+        spoilOn = false
     }
 }
 function quoteFn(text: string): void {
     if (quoteOn === false) {
         text = "> " + text
-        quoteBtn.style.cursor = "default" 
         quoteBtn.style.color = 'gray';
         textArea.value = text
-        
         quoteOn = true
+    } else if(quoteOn === true) {
+        text = text.replace(/> /g, "")
+        quoteBtn.style.color = 'white';
+        textArea.value = text
+        quoteOn = false
     }
 }
 function lineFn(text: string): void {
     if (lineOn === false) {
         text = "`" + text.concat("`")
         textArea.style.fontFamily = "monospace"
-        lineBtn.style.cursor = "default"
         lineBtn.style.color = 'gray';
         textArea.value = text
         lineOn = true
+    } else if(lineOn === true) {
+        text = text.replace(/`/g, "")
+        lineBtn.style.color = 'white';
+        textArea.style.fontFamily = "JetBrains Mono"
+        textArea.value = text
+        lineOn = false
     }
 }
 function blockFn(text: string): void {
     if (blockOn === false) {
         text = "```" + text.concat("```")
         textArea.style.fontFamily = "monospace"
-        blockBtn.style.cursor = "default"
         blockBtn.style.color = 'gray';
         textArea.value = text
         blockOn = true
+    } else if(blockOn === true) {
+        text = text.replace(/```/g, "")
+        blockBtn.style.color = 'white';
+        textArea.style.fontFamily = "JetBrains Mono"
+        textArea.value = text
+        blockOn = false
     }
 }
